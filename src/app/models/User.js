9a70429e6cf791af1947ec51e9b7e0237f44d48e
Gilson <gilsonsantos1998@gmail.com>
2020-07-28 12:43:56 -0300

    aplicando o bcryptjs pra fazer o hash da senha

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,11 +1,14 @@
 import Sequelize, { Model } from 'sequelize';
 
+import bcrypt from 'bcryptjs';
+
 class User extends Model {
   static init(sequelize) {
     super.init(
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL, // não vai existir na base de dados, apenas no código
         password_hash: Sequelize.STRING,
         provider: Sequelize.BOOLEAN,
       },
@@ -13,6 +16,15 @@ class User extends Model {
         sequelize,
       }
     );
+
+    // vai executar antes de salvar as informações de usuário
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8); // estamos aplicando a criptografia da senha do user
+      }
+    });
+
+    return this;
   }
 }
 
