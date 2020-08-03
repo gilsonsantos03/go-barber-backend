751dd7fa67ae790ec8aa84dc5073d3c9ef80b3b2
Gilson <gilsonsantos1998@gmail.com>
2020-08-03 14:53:53 -0300

    aplicando a paginação

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,10 +6,13 @@ import File from '../models/File';
 
 class AppoinmentController {
   async index(req, res) {
+    const { page = 1 } = req.query;
     // o canceled_at null vai mostrar os que ainda não fora cancelados
     const appointnments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
+      limit: 20,
+      offset: (page - 1) * 20,
       attributes: ['id', 'date'],
       include: [
         {
