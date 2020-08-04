5de4f8628fde1d528238559607a850219a13e94b
Gilson <gilsonsantos1998@gmail.com>
2020-08-04 18:04:31 -0300

    configurando nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '6997665d81dac7',
+    pass: '6376b1ff8ce003',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
